f51340cfe58d65ecf09ea1d9776952e9072d4925
Alexie Julien <aljulien@z2r1p3.42lyon.fr>
2024-04-24 17:57:18 +0200

    checked norm, all good

---
+++ split.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 15:31:22 by aljulien          #+#    #+#             */
+/*   Updated: 2024/04/20 18:52:03 by aljulien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+
+static size_t	ft_count_words(const char *str, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (i == 0 || str[i - 1] == c)
+		{
+			if (str[i] != c)
+				count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+static size_t	ft_wordlen(const char *str, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	return (i);
+}
+
+static char	**ft_fill_tab(char const *s, char c, char **tab)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			len = ft_wordlen(&s[i], c);
+			tab[j] = malloc(sizeof(char) * (len + 1));
+			if (!tab[j])
+			{
+				ft_free_tab(tab);
+				return (NULL);
+			}
+			ft_strlcpy(tab[j], &s[i], len + 1);
+			i += len;
+			j++;
+		}
+		else
+			i++;
+	}
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	size;
+	char	**tab;
+
+	if (!s)
+		return (NULL);
+	size = ft_count_words(s, c);
+	tab = malloc(sizeof(char *) * (size + 1));
+	if (!tab)
+		return (NULL);
+	tab = ft_fill_tab(s, c, tab);
+	if (!tab)
+		return (NULL);
+	tab[size] = NULL;
+	return (tab);
+}
